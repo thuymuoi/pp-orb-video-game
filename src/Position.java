deded001f4429bec21db334002e29781642626b0
binputgame1team <binputgame1team@3b9fe9f6-6743-11de-a8c1-9da277d31688>
2007-11-17 21:43:02 +0000

    New Position class for everyone and Player class skeleton

---
+++ src/Position.java
@@ -0,0 +1,17 @@
+
+public class Position {
+	
+	public Integer x;
+	public Integer y;
+	
+	public Position() {
+		x = 0;
+		y = 0;
+	}
+	
+	public Position(int xNew, int yNew){
+		x = xNew;
+		y = yNew;
+	}
+	
+}
