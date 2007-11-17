161e5636db1040f1a0af010655a5866c7a3f9120
binputgame1team <binputgame1team@3b9fe9f6-6743-11de-a8c1-9da277d31688>
2007-11-17 23:05:26 +0000

    Updated player class

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,48 @@
+import java.lang.Math;
+
 
 public class Player {
+	public Position position;
+	public Thruster thruster;
 	
+	//default constructor
+	public Player () {
+		position = new Position (50,150);
+		thruster = new Thruster(position);
+	}
 	
+	public void update(Boolean thrust, Boolean slow){	
+		if(thrust){
+		}
+		else {
+			
+		}
+			
+	}
+	
+	public class Thruster {
+		Position tPos;
+		double angle;
+		double distance;
+		
+		//default constructor
+		public Thruster(Position orbit){
+			angle = 0;
+			tPos = new Position((int)(orbit.x + distance* java.lang.Math.cos(angle)),(int)(orbit.y + distance*java.lang.Math.sin(angle)));
+			
+		}
+		
+		public double getAngle(){
+			return angle;
+		}
+		
+		public void updatePosition(Position orbit){
+			
+						
+		}
+		
+		
+	}//end Thruster class
+
+
 }
