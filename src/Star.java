df35337a8ac7d90bce253b0cbb38a65fba22ebb5
binputgame1team <binputgame1team@3b9fe9f6-6743-11de-a8c1-9da277d31688>
2007-11-18 06:07:12 +0000

    a severed foot is the ultimate stocking stuffer.

--- src/Star.java
+++ src/Star.java
@@ -7,25 +7,23 @@ import javax.imageio.*;
 public class Star {
 	Graphics graphics;
 	private BufferedImage bi;
-	private File f;
 	private int x;
 	private int y;
 	
 	public Star(Graphics graphics, int x, int y){
 		this.graphics = graphics;
 		bi = null;
-		f = null;
 		this.x = x;
 		this.y = y;
 	}
 	
 	public void drawStar(){
 		try {
-			File f = new File("src/Star.jpg");
-			BufferedImage bi = ImageIO.read(f); 
+			bi = ImageIO.read(new File("src/Star.jpg")); 
 		} catch (IOException e){
 			System.out.println("asdf");
 		}
+		graphics = bi.getGraphics();
         graphics.drawImage(bi, x, y, null);
 	}
 	
