08a634a1ddc86bcf15de40460274afe380ab97ba
binputgame1team <binputgame1team@3b9fe9f6-6743-11de-a8c1-9da277d31688>
2007-11-18 06:11:36 +0000

    position with double values

---
+++ src/DPosition.java
@@ -0,0 +1,15 @@
+
+public class DPosition {
+	double x;
+	double y;
+	
+	public DPosition(){
+		x = 0.0;
+		y = 0.0;
+	}
+	
+	public DPosition(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+}
