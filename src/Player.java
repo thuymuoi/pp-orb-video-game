261e50ac0697f56c8d68f0f83795d128377973e2
binputgame1team <binputgame1team@3b9fe9f6-6743-11de-a8c1-9da277d31688>
2007-11-18 22:03:16 +0000

    UPdated thruster speed

--- src/Player.java
+++ src/Player.java
@@ -128,7 +128,7 @@ public class Player {
 		//default constructor
 		public Thruster(Position orbit){
 			angle = 0;
-			angleChange = 0.5;
+			angleChange = 0.35;
 			thrustPosition = new Position((int)(orbit.x + distance* java.lang.Math.cos(angle)),(int)(orbit.y + distance*java.lang.Math.sin(angle)));
 			distance = 15;
 		}
