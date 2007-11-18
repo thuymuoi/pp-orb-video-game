7fc5918c31905d3958ca9584c55bddf80aa66e1d
binputgame1team <binputgame1team@3b9fe9f6-6743-11de-a8c1-9da277d31688>
2007-11-18 03:14:40 +0000

    Good versions

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,4 +1,5 @@
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -8,12 +9,23 @@ import javax.swing.JPanel;
 
 public class GameWindow extends JFrame {
 	JPanel mainPanel;
+	Graphics graphics = null;
 	public GameWindow (){
 		super("Orbit");
 		mainPanel = new JPanel();
 		setContentPane(mainPanel);
 		setVisible(true);
 		setSize(new Dimension(640,480));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		graphics = getGraphics();
+	}
+	public void draw(Player player){
+		graphics.fillOval(player.getMainPosition().x-7, player.getMainPosition().y-7, 14, 14);
+		graphics.drawOval(player.getThrustPosition().x-2, player.getThrustPosition().y-2, 4, 4);
+	}
+	public void clear() {
+		graphics.clearRect(0,0, 640, 480);
+		
 	}
 
 }
