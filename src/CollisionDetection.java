07a34f34a9eaae619710a3a6e699859ac7fd3177
binputgame1team <binputgame1team@3b9fe9f6-6743-11de-a8c1-9da277d31688>
2007-11-18 18:22:51 +0000

    Updated collision detection: bouncing
    - Sherwin

--- src/CollisionDetection.java
+++ src/CollisionDetection.java
@@ -19,11 +19,38 @@ public class CollisionDetection {
 		if (distance > player.getRadius())//make sure we have radius for player
 			return false;
 		else {
+			//Implement player bouncing
+			DPosition wallVector = new DPosition((line.getStartP().x - line.getEndP().x)/line.length(), (line.getStartP().y - line.getEndP().y)/line.length());
+			DPosition wallNormal = new DPosition((line.getStartP().x /2 - (double)player.getMainPosition().x)/length(line.getStartP(), player.getMainPosition()), (line.getStartP().y /2 - (double)player.getMainPosition().y)/length(line.getStartP(), player.getMainPosition()));
 			
+			double t = dotProduct(wallVector, player.getMainPosition());
+			double n = dotProduct(wallNormal, player.getMainPosition());
+	
+			DPosition vt = new DPosition(wallVector.x * t, wallVector.y * t);
+			DPosition vn = new DPosition(wallNormal.x * (-n), wallNormal.y * (-n));
+			
+			player.setSpeedX((int)Math.round(dotProduct(new DPosition(1,0), vn) + dotProduct(new DPosition(1,0), vt)));
+			player.setSpeedY((int)Math.round(dotProduct(new DPosition(0,1), vn) + dotProduct(new DPosition(0,1), vt)));
 			return true;	
+			
 		}
 	}
 	
+	public double dotProduct(DPosition vec1, DPosition vec2){
+		return vec1.x * vec2.x + vec1.y * vec2.y;
+	}
+	
+	public double dotProduct(DPosition vec1, Position vec2){
+		return vec1.x * (double)vec2.x + vec1.y * (double)vec2.y;
+	}
+	
+	public double length (DPosition startP, DPosition endP) {
+		return Math.sqrt(Math.pow(startP.x - endP.x,2) + Math.pow(startP.y - endP.y,2));
+	}
+	
+	public double length (DPosition startP, Position endP) {
+		return Math.sqrt(Math.pow(startP.x - (double)endP.x,2) + Math.pow(startP.y - (double)endP.y,2));
+	}
 	
 	/*
 	 * object center: c
