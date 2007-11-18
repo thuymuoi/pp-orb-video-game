a92edb8e5d9d901e8f3a468519845a898bb4901e
binputgame1team <binputgame1team@3b9fe9f6-6743-11de-a8c1-9da277d31688>
2007-11-18 06:14:05 +0000

    MapLoader update

--- src/MapLoader.java
+++ src/MapLoader.java
@@ -1,12 +1,18 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class MapLoader {
+	ArrayList lineList;
 	public void readFile() throws IOException
 	{
 		BufferedReader input = new BufferedReader(new FileReader("src/map.txt"));
+		double x;
+		double y;
+		double oldx=-96822.96822;
+		double oldy=-96822.96822;
 		while(true){
 			String line = input.readLine();
 			
@@ -14,10 +20,14 @@ public class MapLoader {
 				break;
 			}
 			String[] parts = line.split(" ");
-			double x = Double.valueOf(parts[1].trim());
-			double y = Double.valueOf(parts[2].trim());
-			//double newInput = input.read();
-			System.out.println("x:" + (x+116) + " y:" + (y+175));
+			x = Double.valueOf(parts[1].trim());
+			y = Double.valueOf(parts[2].trim());
+			if(oldx != -96822.96822 && oldy != -96822.96822){
+				System.out.println("x:" + (x+116) + " y:" + (y+175)+"oldx:" + (oldx+116) + " oldy:" + (oldy+175));
+				lineList.add(new Line(new DPosition(oldx,oldy),new DPosition(x,y)));
+			}
+			oldx = x;
+			oldy = y;
 		}
 
 	
