56732beda5da79d0785773ae7963e8dab2e09c79
binputgame1team <binputgame1team@3b9fe9f6-6743-11de-a8c1-9da277d31688>
2007-11-18 19:32:24 +0000

    Charlene - thruster flicker

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -21,11 +21,15 @@ public class GameWindow extends JFrame {
 		graphics = getGraphics();
 	}
 	public void draw(Player player){
+		double tailExtension;
+		tailExtension = Math.random() * 10; 
 		graphics.fillOval(player.getMainPosition().x-7, player.getMainPosition().y-7, 14, 14);
 		graphics.drawOval(player.getThrustPosition().x-2, player.getThrustPosition().y-2, 4, 4);
 		if(Orbit.key1 == true)
 		{
-			graphics.drawLine(player.getThrustPosition().x, player.getThrustPosition().y,  (int) (player.getThrustPosition().x-2+Math.cos(player.getThrusterAngle())*20), (int) (player.getThrustPosition().y-2+Math.sin(player.getThrusterAngle())*20));
+			graphics.drawLine(player.getThrustPosition().x, player.getThrustPosition().y,  (int) (player.getThrustPosition().x-2+Math.cos(player.getThrusterAngle())*(10+tailExtension)), (int) (player.getThrustPosition().y-2+Math.sin(player.getThrusterAngle())*(10+tailExtension)));
+			graphics.drawLine(player.getThrustPosition().x-2, player.getThrustPosition().y-2,  (int) (player.getThrustPosition().x-2+Math.cos(player.getThrusterAngle())*(10+tailExtension)), (int) (player.getThrustPosition().y-2+Math.sin(player.getThrusterAngle())*(10+tailExtension)));
+			graphics.drawLine(player.getThrustPosition().x+2, player.getThrustPosition().y+2,  (int) (player.getThrustPosition().x-2+Math.cos(player.getThrusterAngle())*(10+tailExtension)), (int) (player.getThrustPosition().y-2+Math.sin(player.getThrusterAngle())*(10+tailExtension)));
 		}
 	}
 	public void clear() {
