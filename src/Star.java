e725dca61b6edfd7825e707dd2780ea0cf452873
binputgame1team <binputgame1team@3b9fe9f6-6743-11de-a8c1-9da277d31688>
2007-11-18 05:34:09 +0000

    brand spank'n new baby!!!

--- src/Star.java
+++ src/Star.java
@@ -1,11 +1,30 @@
+import java.io.*;
+import java.util.*;
 import java.awt.Graphics;
-
+import java.awt.image.*;
+import javax.imageio.*;
 
 public class Star {
 	Graphics graphics;
-	public Star(Graphics graphics, Integer x, Integer y){
+	private BufferedImage bi;
+	private File f;
+	private int x;
+	private int y;
+	
+	public Star(Graphics graphics, int x, int y){
 		this.graphics = graphics;
+		this.x = x;
+		this.y = y;
 	}
 	
+	public void drawStar(){
+		try {
+			File f = new File("Star.jpg");
+			BufferedImage bi = ImageIO.read(f); 
+		} catch (IOException e){
+			
+		}
+        graphics.drawImage(bi, x, y, null);
+	}
 	
 }
