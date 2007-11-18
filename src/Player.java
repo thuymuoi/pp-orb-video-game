7493052217059f3a5801f12287ae0ad27f36f96b
binputgame1team <binputgame1team@3b9fe9f6-6743-11de-a8c1-9da277d31688>
2007-11-18 01:12:06 +0000

    m

--- src/Player.java
+++ src/Player.java
@@ -10,7 +10,7 @@ public class Player {
 		//speed = 5;		//Original speed idea
 		
 		speed = new Position(0,0);
-		speedMagnitude = 5;
+		speedMagnitude = 2;
 		mainPosition = new Position (50,150);
 		thruster = new Thruster(mainPosition);
 	}
