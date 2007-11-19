968110cc062b3acc69e0209aaffd456ddd113c84
binputgame1team <binputgame1team@3b9fe9f6-6743-11de-a8c1-9da277d31688>
2007-11-19 01:56:53 +0000

    Charlene - sound class comments

--- src/Sound.java
+++ src/Sound.java
@@ -1,37 +1,46 @@
 import java.io.File;
-
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.SourceDataLine;
 
-//import Sound.PlayThread;
-
+/**
+ * Implements the playing and stopping of sounds
+ * @author Owner
+ *
+ */
 public class Sound {
 	AudioFormat audioFormat;
 	AudioInputStream audioInputStream;
 	SourceDataLine sourceDataLine;
 	Boolean playback;
 	
+	/**
+	 * Constructor
+	 */
 	public Sound()
 	{
 		playback = false;
 	}
 	
+	/**
+	 * Plays audio file 
+	 * @param fileName Sound file to be played
+	 */
 	public void playAudio(String fileName)
 	{
 		try
 		{
-		File mySoundFile = new File(fileName);
-		audioInputStream = AudioSystem.getAudioInputStream(mySoundFile);
-		audioFormat = audioInputStream.getFormat();
-		//System.out.println(audioFormat);
-		DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class,audioFormat);
-		sourceDataLine = (SourceDataLine)AudioSystem.getLine(dataLineInfo);
-		playback = true;
-		
-		new PlayThread().start();
+			File mySoundFile = new File(fileName);
+			audioInputStream = AudioSystem.getAudioInputStream(mySoundFile);
+			audioFormat = audioInputStream.getFormat();
+			
+			DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class,audioFormat);
+			sourceDataLine = (SourceDataLine)AudioSystem.getLine(dataLineInfo);
+			playback = true;
+			
+			new PlayThread().start();
 		}
 		catch(Exception e)
 		{
@@ -40,11 +49,19 @@ public class Sound {
 		}
 	}
 	
+	/**
+	 * Stop playback of audio
+	 */
 	public void stopAudio()
 	{
 		playback = false;
 	}
 	
+	/**
+	 * Inner class for making a separate thread to run the sound
+	 * @author Owner
+	 *
+	 */
 	class PlayThread extends Thread{
 		byte tempBuffer[] = new byte[10000];
 		
